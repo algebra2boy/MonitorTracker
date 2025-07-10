650ab1d3092db4734820010aff7f8197574bd497
yongyetan1209 <yongyetan1209@gmail.com>
2025-07-09 23:37:04 -0400

    Initial Commit

---
+++ MonitorTracker/MonitorTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorTrackerApp.swift
+//  MonitorTracker
+//
+//  Created by Yongye Tan on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MonitorTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
